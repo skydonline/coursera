f1722cdf2d3594cc46d9494bbfe4efca538773fd
Sky Deng <sky.deng.online@outlook.com>
2023-09-08 09:45:24 -0400

    first RN component

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#495E57",
+            }}
+        >
+            <LittleLemonHeader />
+            <LittleLemonFooter />
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
