ce1655f978cb1d0b2a705cd4ad91a6ceeea8856c
Sky Deng <sky.deng.online@outlook.com>
2023-09-08 20:49:53 -0400

    changed testing to one folder

--- rn-sample/App.js
+++ rn-sample/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
+
 export default function App() {
     return (
         <>
